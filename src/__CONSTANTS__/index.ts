33fbaa7e3b37e499f91a2be0b1cc94d9143f9336
Muhammad Giwa <mgiwa78@gmail.com>
2024-04-08 00:57:34 +0100

    Baseline

--- src/__CONSTANTS__/index.ts
+++ src/__CONSTANTS__/index.ts
@@ -5,7 +5,10 @@ export const PAGE_SIZE = 10;
 
 process.env.NODE_ENV === "development";
 
-const MONGO_URI = "mongodb://localhost:27017/job-application";
+const MONGO_URI =
+  process.env.NODE_ENV === "development"
+    ? "mongodb://localhost:27017/job-application"
+    : "mongodb+srv://vercel-admin-user:TpcUDU37xA5JroSR@cluster0.za7xrpe.mongodb.net/job-application";
 
 console.log(MONGO_URI);
 export { JWT_SECRET, MONGO_URI };
